e20ebfa7985b486c9fd63b34e3087d214c4d986c
Iswenzz <suxlolz1528@gmail.com>
2021-10-13 16:57:14 +0200

    Tests: Unit tests for DLL injection/ejection

--- src/tests/IzInjector.test.c
+++ src/tests/IzInjector.test.c
@@ -1,12 +1,62 @@
+#pragma warning(disable: 6335)
+#include "IzInjector.h"
+
 #include <greatest.h>
+#include <Windows.h>
 
-TEST test() 
+PROCESS_INFORMATION OpenNotepad()
 {
+    STARTUPINFO si = { 0 };
+    PROCESS_INFORMATION pi = { 0 };
+    si.cb = sizeof(si);
 
-    ASSERT_EQ(1, 1);
+    CreateProcess(NULL, "\"notepad.exe\"", NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+    return pi;
+}
+
+void CloseNotepad(PROCESS_INFORMATION pi)
+{
+    TerminateProcess(pi.hProcess, 0);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+}
+
+void SyncDestroyDialog()
+{
+    while (TRUE)
+    {
+        HWND iHandle = FindWindow(NULL, "DLL");
+        if (iHandle)
+        {
+            SendMessage(iHandle, WM_SYSCOMMAND, SC_CLOSE, 0);
+            break;
+        }
+        Sleep(200);
+    }
+}
+
+TEST test_inject() 
+{
+    LPPROCESS_INFORMATION proc = Inject("notepad.exe", 0, "IzInjectorFixture.dll", TRUE);
+    SyncDestroyDialog();
+    ASSERT(proc);
+}
+
+TEST test_eject()
+{
+    LPPROCESS_INFORMATION proc = Eject("notepad.exe", 0, "IzInjectorFixture.dll", FALSE, TRUE);
+    SyncDestroyDialog();
+    ASSERT(proc);
 }
 
 SUITE(Suite_IzInjector)
 {
-    RUN_TEST(test);
+    // Fixture DLL location and notepad for testing it
+    SetCurrentDirectory("bin");
+    PROCESS_INFORMATION process = OpenNotepad();
+
+    RUN_TEST(test_inject);
+    RUN_TEST(test_eject);
+
+    CloseNotepad(process);
 }
