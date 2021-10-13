122b305612cf3227e27ba8b2253f01fb2b83c21c
Iswenzz <suxlolz1528@gmail.com>
2021-10-13 16:56:58 +0200

    Fixture: Removed UNICODE

--- fixture/dllmain.c
+++ fixture/dllmain.c
@@ -1,14 +1,21 @@
 #include <Windows.h>
 
+/// <summary>
+/// DLL Entry point.
+/// </summary>
+/// <param name="hModule">DLL module handle.</param>
+/// <param name="ul_reason_for_call">Reason for calling this function.</param>
+/// <param name="lpReserved">Indicates whether the DLL is being loaded statically or dynamically.</param>
+/// <returns></returns>
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
 		case DLL_PROCESS_ATTACH:
-			MessageBox(NULL, L"DLL Injection Succeed!", L"DLL", MB_OK | MB_ICONINFORMATION);
+			MessageBox(NULL, "DLL Injection Succeed!", "DLL", MB_OK | MB_ICONINFORMATION);
 			break;
 		case DLL_PROCESS_DETACH:
-			MessageBox(NULL, L"DLL Ejection Succeed!", L"DLL", MB_OK | MB_ICONINFORMATION);
+			MessageBox(NULL, "DLL Ejection Succeed!", "DLL", MB_OK | MB_ICONINFORMATION);
 			break;
 		case DLL_THREAD_ATTACH:
 		case DLL_THREAD_DETACH:
